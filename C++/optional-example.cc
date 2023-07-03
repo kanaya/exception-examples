e4ac587565077f26a2310b5ecaeeb0ebf5530e95
Ichi Kanaya <kanaya@pineapple.cc>
2023-07-03 19:18:32 +0900

    Added optional-example.cc

---
+++ C++/optional-example.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <optional>
+
+template <typename T> std::optional<T> divide(T y, T x) noexcept {
+  if (x != 0.0) {
+    return {y/x};
+  }
+  else {
+    return {};
+  }
+}
+
+int main(void) {
+  float x = 0.0;
+  float y = 2.0;
+  auto z = divide(y, x);
+  if (z) {
+    std::cout << z.value() << std::endl;
+  }
+  else {
+    std::cerr << "Devide by zero" << std::endl;
+  }
+  return 0;
+}
+
